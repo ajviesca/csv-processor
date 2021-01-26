c732f200f84cac13b6cdebce04f71a0102f76b7b
Adrian Viesca <Adrian.Joseph.Viesca@ing.com>
2021-01-26 22:36:08 +0800

    add initial csv reader test

---
+++ src/test/groovy/com/viesca/csvprocessor/service/CsvReaderSpec.groovy
@@ -0,0 +1,22 @@
+package com.viesca.csvprocessor.service
+
+
+import spock.lang.Specification
+
+class CsvReaderSpec extends Specification {
+
+    def sut;
+
+    def setupSpec() {
+        sut = new CsvReader();
+    }
+
+    def "should read csv files from a given directory"() {
+        given: "a given directory"
+        def directory = 'src/test/resources'
+        when:
+        def files = sut.getFiles(directory)
+        then:
+        files.size == 2
+    }
+}
