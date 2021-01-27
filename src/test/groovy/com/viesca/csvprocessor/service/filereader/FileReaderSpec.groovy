2d7381c2951a8e25f5f0ee1fd3bf7c30a97edd0d
Adrian Viesca <Adrian.Joseph.Viesca@ing.com>
2021-01-27 22:06:14 +0800

    uncomment FileReader unit test

--- src/test/groovy/com/viesca/csvprocessor/service/filereader/FileReaderSpec.groovy
+++ src/test/groovy/com/viesca/csvprocessor/service/filereader/FileReaderSpec.groovy
@@ -7,17 +7,18 @@ import spock.lang.Unroll
 
 class FileReaderSpec extends Specification {
 
-//    @Unroll
-//    def "#reader :: should read #fileType files from a given directory"() {
-//        given: "a given directory"
-//        def directory = 'src/test/resources'
-//        when:
-//        def files = reader.getFiles(directory)
-//        then:
-//        files.size() == resultSize
-//        where:
-//        reader            | fileType || resultSize
-//        new CsvReader()   | 'csv'    || 2
-//        new JSONReader() || 'json'   || 1
-//    }
+    @Unroll
+    def "#reader :: should read #fileType files from a given directory"() {
+        given: "a given directory"
+        def directory = 'src/test/resources'
+        when:
+        def files = reader.getFiles(directory)
+        then:
+        files.size() == resultSize
+        where:
+        reader           | fileType || resultSize
+        new CsvReader()  | 'csv'    || 2
+        new JSONReader() | 'json'   || 1
+        new XMLReader()  | 'xml'    || 1
+    }
 }
